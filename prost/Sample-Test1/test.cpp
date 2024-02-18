3231212df4f60c26c05023c0027d0633197a488f
aslitvinenko <oven034@mail.ru>
2024-02-18 17:32:38 +0300

    Signed-off-by: aslitvinenko <oven034@mail.ru>

---
+++ prost/Sample-Test1/test.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "../StaticLib1/framework.h"
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ (prost(11), true);
+  EXPECT_TRUE (prost(11));
+}
---
+++ prost/StaticLib1/StaticLib1.cpp
@@ -0,0 +1,21 @@
+﻿// StaticLib1.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include <cmath>
+int prost(int n)
+{
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            // вывести, что n  не простое, так как делится на i
+            return false;
+        }
+    }
+    //вывести что n простое. 
+    return true;
+}
+// TODO: Это пример библиотечной функции.
+void fnStaticLib1()
+{
+}
